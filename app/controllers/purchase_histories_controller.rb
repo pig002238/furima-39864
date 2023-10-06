2bcbb30a94fb81ed38a45d7c5cce6c04fb9a4807
pig002238 <144104624+pig002238@users.noreply.github.com>
2023-10-06 13:09:34 +0900

    エラーが表示されるようにする

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -5,31 +5,28 @@ class PurchaseHistoriesController < ApplicationController
   end
 
   def create
-    @purchase_history = Purchase.new(purchase_params)
-    if @purchase_history.save
-      # 購入が成功した場合の処理
-      # 配送先情報を保存する場合の例:
-      @delivery_address = DeliveryAddress.new(delivery_address_params)
-      @delivery_address.purchase = @purchase_history
-      if @delivery_address.save
-        redirect_to root_path, notice: '購入が完了しました。'
-      else
-        # 配送先情報の保存に失敗した場合の処理
-        # エラーメッセージを表示するか、エラーページにリダイレクトするなどの対応が必要
-        render :new
-      end
+    @purchase_form = PurchaseForm.new(purchase_params)
+    if @purchase_form.valid?
+      @purchase_form.save(params,current_user.id)
+      redirect_to root_path
     else
-      render :new
+      @item = Item.find(params[:item_id])
+      render :index
     end
   end
-
+  
   private
 
-  def purchase_params
-    params.require(:purchase_form).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :street_number, :building, :phone_number)
-  end
-end
+    def purchase_params
+      params.require(:purchase_form).permit(
+        :postal_code,
+        :prefecture_id,
+        :city,
+        :street_number,
+        :building,
+        :phone_number,
+      ).merge(item_id: params[:item_id], user_id: current_user.id)
+    end
+    
 
-def delivery_address_params
-  params.require(:delivery_address).permit(:postal_code, :prefecture_id, :city, :street_number, :building, :phone_number)
 end
--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,18 +1,29 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_number, :phone_number, :purchase_history
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_number, :building, :phone_number
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id
     validates :city
     validates :street_number
-    validates :phone_number
-    validates :purchase_history
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :item_id
+    validates :user_id
   end
+  
 
-  def save
-    purchase_history = PurchaseHistory.create(item_id: item_id, user_id: user_id)
-    DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_number: street_number, building: building, purchase_history: purchase_history)
+  def save(params,user_id)
+    purchase_history = PurchaseHistory.create(item_id: params[:item_id].to_i, user_id: user_id)
+    
+    DeliveryAddress.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      street_number: street_number,
+      building: building,
+      phone_number: phone_number, 
+      purchase_history_id: purchase_history.id
+    )
   end
 end
